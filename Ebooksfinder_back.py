ed10cde6586f4d7cedc34d64a13b1686c04cea99
Shieber <QMH_XB_FLTMY@yahoo.com>
2019-01-04 13:10:57 +0800

    first commit

---
+++ Ebooksfinder_back.py
@@ -0,0 +1,156 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# 自动获取电子书中作者提到的所有书籍
+# 保存在文件中 '20xxbooknames.txt'
+
+import re
+import os,sys
+import shutil 
+import zipfile
+from send2trash import send2trash
+reload(sys)
+sys.setdefaultencoding('utf-8')                         #解决中文解析出错的问题
+
+class EPUB_books_finder():
+	'''
+		epub,mobi电子书中推荐书籍获取器，通过命令行传入电子书名称
+	    程序自动查找书中作者推荐的所有以中文书名号《》括起来的书籍。
+	'''
+	def __init__(self):
+		'''初始化程序运行的信息'''
+		self.delimiter = '/'                            #目录分隔符,windows是\
+		self.html_list = []                             #全局变量，保存(x)html的具体路径
+		self.epub_name = sys.argv[1]                    #初始化电子书名
+		self.book_suffix = '_booklist.txt'              #书单的后缀名,可自行设置
+		self.book_prefix = self.get_epub_prefix(sys.argv[1])     #电子书前缀名
+		self.booklist_name = self.book_prefix + self.book_suffix #书单名称
+
+	def get_epub_prefix(self,file_name):
+		'''提取电子书或任意文件的前缀名'''
+		name_pattern = re.compile(r'(.*?)\.')
+		try:
+			matched = name_pattern.findall(file_name)   #文件名，书名，不含点和后缀
+			prefix  = matched[0]
+		except Exception as err:                        #except IndexError:
+			return False
+		return prefix                                   #既是电子书前缀名也是解压后的文件夹名
+
+	def copy2zipdir(self,zip_dir):
+		'''为epub建立同名文件夹,并把电子书拷贝进去'''
+		os.makedirs(zip_dir)
+		new_name = zip_dir + self.delimiter +  self.epub_name 
+		shutil.copy(self.epub_name, new_name)           #注意:windows下delimiter是\
+
+	def rename_epub2zip(self,epub_name):
+		'''重命名电子书为zip格式并提取到当前文件夹'''
+		prefix = self.get_epub_prefix(epub_name)
+		if not prefix: 
+			sys.exit(-1)
+
+		zip_name = prefix + '.zip'                      #zip名称
+		if epub_name.endswith('.epub') or epub_name.endswith('.mobi'):
+			shutil.move(epub_name,zip_name)
+			return zip_name
+		else:
+			return False
+
+	def unzip_file(self,zip_name):
+		'''解压电子书的zip格式为文件夹,文件夹名为电子书名'''
+		if not zip_name:
+			sys.exit(-1)
+		try:
+			with zipfile.ZipFile(zip_name) as zipObj:
+				zipObj.extractall()
+			success = True
+		except Exception as err:
+			success = False	
+
+		if not success:
+			sys.exit(-1)
+
+	def isxhtml(self,filename):
+		'''判断是否是html或者xhtml,既电子书的文字文档'''
+		if filename.endswith('.html') or filename.endswith('.xhtml'):
+			return True
+		else:
+			return False
+
+	def find_xhtml(self,zipdir):
+		'''递归式的查找xhtml文件并添加到列表中'''
+		for parent,dirnames,filenames in os.walk(zipdir):
+			for dirname in  dirnames:        
+				self.find_xhtml(dirname)
+			for filename in filenames:    
+				if self.isxhtml(filename):
+					html_file = os.path.join(parent,filename)
+					self.html_list.append(html_file)
+
+	def write2booklist(self,books):
+		'''将所有匹配到的书籍写入文件，一行一本'''
+		booklist_Obj  = open(self.booklist_name,'a+')
+		for b in range(len(books)):
+			booklist_Obj.write(books[b]+ '\n')
+		booklist_Obj.close()
+
+	def find_book(self,html_file):
+		'''在一个html文件中查找书籍'''
+		html_Obj = open(html_file)
+		html_txt = html_Obj.read()
+		html_Obj.close()
+		book_pat = re.compile(r'(《.*?》)')
+		books    = book_pat.findall(html_txt)
+
+		if 0 != len(books):
+			self.write2booklist(books)
+
+	def rmduplicate(self):
+		'''去除重复记录的书籍'''
+		if os.path.exists(self.booklist_name)
+			send2trash(zip_dir)                         #没有找到推荐书籍，删除解压缩得到的文件夹　
+			sys.exit(0)
+
+		books = []
+		books_Obj = open(self.booklist_name) 
+		lines = books_Obj.readlines()
+		for line in lines:
+			book = line.strip('\n')
+			books.append(book)
+		books_Obj.close()
+
+		books = sorted(set(books), key=books.index)     #去重
+
+		books_Obj = open(self.booklist_name,'w+') 
+		for b in range(len(books)):
+			books_Obj.write(books[b]+ '\n')             #再次写入
+		books_Obj.close()
+
+	def search_reserve_books(self):  
+		'''总函数，直接调用这一个就可'''
+		if len(sys.argv) != 2:
+			print("Usage: python %s [xx.epub|xx.mobi]"%sys.argv[0])
+			sys.exit(-1)
+
+		zip_dir = self.get_epub_prefix(self.epub_name)  #获取电子书名以创建同名文件夹
+		self.copy2zipdir(zip_dir)                       #复制电子书到同名文件夹 
+
+		os.chdir(zip_dir)                               #切换到文件夹,开始处理电子书  
+		zip_name = self.rename_epub2zip(self.epub_name) #改电子书格式为zip 
+		self.unzip_file(zip_name)                       #解压缩zip文件
+
+		for item in os.listdir('.'):                     
+			if os.path.isdir(item):                     #是路径就进入查找
+				self.find_xhtml(item)                   #查找同名文件夹下的(x)html文件，核心函数
+
+		os.chdir('../')                                 #退回当前目录,开始查找书籍任务
+		for html_file in self.html_list:
+			self.find_book(zip_dir + self.delimiter + html_file)  
+			                                            #在(x)html中找书，有则写入文件，核心函数
+		self.rmduplicate()                              #去除重复记录的书籍
+		send2trash(zip_dir)                             #删除解压缩得到的文件夹　
+
+if __name__ == "__main__":
+	if len(sys.argv) != 2:
+		print("Usage: python %s [xx.epub|xx.mobi]"%sys.argv[0])
+		sys.exit(-1)
+	book_finder = EPUB_books_finder()
+	book_finder.search_reserve_books()
---
+++ README.rst
@@ -0,0 +1,34 @@
+Ebooksfinder
+=============
+Command line interface for finding books recommended by author in an ebook(epub or mobi type).
+
+Versions
+--------
+Ebooksfinder works with Python 2.7
+
+Installation
+------------
+
+Github
+~~~~~~
+
+::
+
+    pip install git+https://github.com/QMH-TMY/Ebooksfinder.git
+
+or
+
+::
+
+    git clone https://github.com/QMH-TMY/Ebooksfinder.git
+    python Ebooksfinder/setup.py install
+
+Usage
+-----
+
+::
+
+    $ Ebooksfinder [yourebook.epub | yourebook.mobi]
+
+		Just for fun!
+			Jan 4,2019, Chengdu, China.
