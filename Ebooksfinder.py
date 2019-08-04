6521d7ca3a49cda3f4bfa45a39cc376518fa2ccf
Shieber <QMH_XB_FLTMY@yahoo.com>
2019-08-04 18:26:14 +0800

    modify to py3

--- Ebooksfinder.py
+++ Ebooksfinder.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python2
+#!/usr/bin/python3
 # -*- coding: utf-8 -*-
 #
 #    Author: Shieber
@@ -21,9 +21,10 @@
 #    Copyright 2019 
 #    All Rights Reserved!
 
+import re,sys
 from os import makedirs
 from os import walk 
-from os import chdir
+from os import chdir,getcwd
 from os import listdir
 from shutil  import move  
 from shutil  import copy
@@ -33,9 +34,7 @@ from os.path    import  isdir
 from os.path    import  exists 
 from os.path    import  basename 
 from send2trash import send2trash
-from re import compile as Compile 
 
-__version__ = '0.1'
 
 class EPUBbooksfinder():
     '''
@@ -43,57 +42,51 @@ class EPUBbooksfinder():
         程序自动查找书中作者推荐的所有以中文书名号《》括起来的书籍。
         通过使用-a命令行参数，自动查找当前目录下的所有电子书推荐书籍。
     '''
-    def __init__(self):
+    def __init__(self,suffix='_booklist.txt'):
         '''初始化程序运行的信息'''
-        self.delimiter = '/'                            #目录分隔符,windows是\
+        self.delimiter = '/'                            #目录分隔符,windows下是\
         self.html_list = []                             #全局变量，保存(x)html的具体路径
         self.epub_name = sys.argv[1]                    #初始化电子书名
-        self.book_suffix = '_booklist.txt'              #书单的后缀名,可自行设置
-        self.book_prefix = self.get_epub_prefix(sys.argv[1])     #电子书前缀名
-        self.booklist_name = self.book_prefix + self.book_suffix #书单名称
+        self.suffix    = suffix                         #书单的后缀名,可自行设置
+        self.prefix    = self.get_epub_prefix(sys.argv[1]) #电子书前缀名
+        self.booklist  = self.prefix + self.suffix      #书单名称
 
     def get_epub_prefix(self,file_name):
         '''提取电子书或任意文件的前缀名'''
-        name_pattern = Compile(r'(.*?)\.')
-        try:
-            matched = name_pattern.findall(file_name)   #文件名，书名，不含点和后缀
-            prefix  = matched[0]
-        except Exception as err:                        #except IndexError:
-            return False
-        return prefix                                   #解压后的文件夹名
+        return file_name.split('.')[0]                  #解压后的文件夹名
 
     def copy2zipdir(self,zip_dir):
         '''为epub建立同名文件夹,并把电子书拷贝进去'''
         makedirs(zip_dir)
         new_name = zip_dir + self.delimiter +  self.epub_name 
-        copy(self.epub_name, new_name)                  #注意:windows下delimiter是\
+        copy(self.epub_name, new_name)                  
 
-    def rename_epub2zip(self,epub_name):
+    def rename_epub2zip(self,name):
         '''重命名电子书为zip格式并提取到当前文件夹'''
-        prefix = self.get_epub_prefix(epub_name)
+        prefix = self.get_epub_prefix(name)
         if not prefix: 
             sys.exit(-1)
 
-        zip_name = prefix + '.zip'                      #zip名称
-        if epub_name.endswith('.epub') or epub_name.endswith('.mobi'):
-            move(epub_name,zip_name)
+        zip_name = prefix + '.zip'                      
+        if name.endswith('.epub') or name.endswith('.mobi'):
+            move(name,zip_name)
             return zip_name
         else:
             return False
 
-    def unzip_file(self,zip_name):
+    def unzip_file(self,name):
         '''解压电子书的zip格式为文件夹,文件夹名为电子书名'''
-        if not zip_name:
+        if not name:
             sys.exit(-1)
         try:
-            with ZipFile(zip_name) as zipObj:
+            with ZipFile(name) as zipObj:
                 zipObj.extractall()
         except Exception as err:
             sys.exit(-1)
 
-    def isxhtml(self,filename):
+    def isxhtml(self,name):
         '''判断是否是html或者xhtml,既电子书的文字文档'''
-        if filename.endswith('.html') or filename.endswith('.xhtml'):
+        if name.endswith('.html') or name.endswith('.xhtml'):
             return True
         else:
             return False
@@ -110,38 +103,36 @@ class EPUBbooksfinder():
 
     def write2booklist(self,books):
         '''将所有匹配到的书籍写入文件，一行一本'''
-        booklist_Obj  = open(self.booklist_name,'a+')
-        for b in range(len(books)):
-            booklist_Obj.write(books[b]+ '\n')
-        booklist_Obj.close()
+        with open(self.booklist,'a+') as booklist_Obj:
+            for i in range(len(books)):
+                booklist_Obj.write(books[i]+ '\n')
 
     def find_book(self,html_file):
         '''在一个html文件中查找书籍'''
-        html_Obj = open(html_file)
-        html_txt = html_Obj.read()
-        html_Obj.close()
-        book_pat = Compile(r'(《.*?》)')
-        books    = book_pat.findall(html_txt)
+        with open(html_file) as html_Obj:
+            html_txt = html_Obj.read()
 
-        if 0 != len(books):
+        book_pat = re.compile(r'(《.*?》)')
+        books    = book_pat.findall(html_txt)
+        if books:
             self.write2booklist(books)
 
     def rmduplicate(self):
         '''去除重复记录的书籍'''
-        if not exists(self.booklist_name):
+        if not exists(self.booklist):
             return                                      #没有则返回
 
         books = []
-        with open(self.booklist_name) as books_Obj:
+        with open(self.booklist) as books_Obj:
             lines = books_Obj.readlines()
             for line in lines:
                 book = line.strip('\n')
                 books.append(book)
 
         books = sorted(set(books), key=books.index)     #去重
-        with open(self.booklist_name,'w+') as books_Obj:
-            for b in range(len(books)):
-                books_Obj.write(books[b]+ '\n')         #再次写入
+        with open(self.booklist,'w+') as books_Obj:
+            for i in range(len(books)):
+                books_Obj.write(books[i]+ '\n')         #再次写入
 
     def search(self):
         '''功能函数，直接调用这一个就可'''
@@ -149,6 +140,7 @@ class EPUBbooksfinder():
             print("Usage: python %s [xx.epub|xx.mobi]"%sys.argv[0])
             sys.exit(-1)
 
+        cwd = getcwd()
         zip_dir = self.get_epub_prefix(self.epub_name)  #获取电子书名以创建同名文件夹
         self.copy2zipdir(zip_dir)                       #复制电子书到同名文件夹 
 
@@ -159,11 +151,10 @@ class EPUBbooksfinder():
         for item in listdir('.'):                     
             if isdir(item):                             #是路径就进入查找
                 self.find_xhtml(item)                   #查找(x)html文件，核心函数
-
             if self.isxhtml(item):
                 self.html_list.append(item)
 
-        chdir('../')                                    #退回当前目录,开始查找书籍任务
+        chdir(cwd)                                      #退回当前目录,开始查找书籍任务
         for html_file in self.html_list:
             self.find_book(zip_dir+self.delimiter+html_file) #在(x)html中找书，核心函数  
                                                             
