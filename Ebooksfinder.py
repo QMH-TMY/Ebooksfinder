b7073462559b70350765bfe7dbb917850d7a6a74
Shieber <QMH_XB_FLTMY@yahoo.com>
2020-07-24 19:12:43 +0800

    modify code

--- Ebooksfinder.py
+++ Ebooksfinder.py
@@ -7,6 +7,7 @@
 import sys
 import re
 import platform
+import click
 from zipfile import ZipFile
 from shutil  import move,copy
 from send2trash import send2trash
@@ -35,101 +36,93 @@ class EPUBbooksfinder():
         self.htmlLst   = []                           #全局变量，保存(x)html的具体路径
         self.epubName  = epubName                     #初始化电子书名
         self.suffix    = suffix                       #书单的后缀名,可自行设置
-        self.prefix    = self.getPrefix(sys.argv[1])  #电子书前缀名
+        self.prefix    = self.getPrefix(self.epubName)#电子书前缀名
         self.booklist  = self.prefix + self.suffix    #书单名称
         self.pattern   = re.compile(r'(《.*?》)')     #《xxx》正则匹配式
 
-    def getPrefix(self,name):
+    def getPrefix(self, ebook):
         '''提取电子书或任意文件的前缀名'''
-        return name.split('.')[0]                     #解压后的文件夹名
+        return ebook.split('.')[0]                     #解压后的文件夹名
 
-    def copy2zipdir(self,zipDir):
+    def copy2zipdir(self, zipDir):
         '''为epub建立同名文件夹,并把电子书拷贝进去'''
         makedirs(zipDir)
         newName = zipDir + self.delimiter +  self.epubName 
         copy(self.epubName, newName)
 
-    def renameEpub2zip(self,name):
+    def renameEpub2zip(self, ebook):
         '''重命名电子书为zip格式并提取到当前文件夹'''
-        prefix = self.getPrefix(name)
+        prefix = self.getPrefix(ebook)
         if not prefix: 
             sys.exit(-1)
 
         zipName = prefix + '.zip'                      
-        if name.endswith('.epub') or name.endswith('.mobi'):
-            move(name,zipName)
+        if ebook.endswith('.epub') or ebook.endswith('.mobi'):
+            move(ebook, zipName)
             return zipName
         else:
             return False
 
-    def unzip(self,name):
+    def unzip(self, ebook):
         '''解压电子书zip格式,目录名为电子书名'''
-        if not name:
+        if not ebook:
             sys.exit(-1)
 
         try:
-            with ZipFile(name) as zipObj:
-                zipObj.extractall()
+            with ZipFile(ebook) as obj:
+                obj.extractall()
         except Exception as err:
             sys.exit(-1)
 
-    def isXhtml(self,name):
-        '''判断是否是htm, html或者xhtml,即电子书文档格式'''
-        return name.endswith('.html') or name.endswith('.xhtml') or name.endswith('.htm')
+    def isXhtml(self, fl):
+        '''判断是否是htm, html或xhtml'''
+        return fl.endswith('.html') or fl.endswith('.xhtml') or fl.endswith('.htm')
 
-    def findXhtml(self,zipdir):
+    def findXhtml(self, zipdir):
         '''递归查找所有xhtml文件并添加到列表中'''
-        for parent,dirnames,filenames in walk(zipdir):
-            for dirname in  dirnames:        
-                self.findXhtml(dirname)
+        for parent, subfolders, filenames in walk(zipdir):
+            for subfolder in subfolders:        
+                self.findXhtml(subfolder)
             for filename in filenames:    
                 if self.isXhtml(filename):
-                    html = join(parent,filename)
-                    self.htmlLst.append(html)
+                    abshtmlname = join(parent, filename)
+                    self.htmlLst.append(abshtmlname)
 
     def findBook(self, html):
         '''在一个html文件中查找书籍'''
-        with open(html) as Obj:
-            txt = Obj.read()
+        with open(html,'r',encoding='utf-8') as obj:
+            data = obj.read()
 
-        books = self.pattern.findall(txt)
+        books = self.pattern.findall(data)
         if books:
             self.write2booklist(books)
 
-    def write2booklist(self,books):
+    def write2booklist(self, books):
         '''将所有匹配到的书籍写入文件，一行一本'''
-        with open(self.booklist,'a+') as bklstObj:
+        with open(self.booklist,'a+', encoding='utf-8') as obj:
             for book in books:
-                bklstObj.write(book + '\n')
+                obj.write(book + '\n')
 
     def rmduplicate(self):
         '''去除重复记录的书籍'''
         if not exists(self.booklist):
             return                                    #没有则返回
 
-        books = []
-        with open(self.booklist) as Obj:
-            lines = Obj.readlines()
-            for line in lines:
-                book = line.strip('\n')
-                books.append(book)
+        books = set()
+        with open(self.booklist,'r',encoding='utf-8') as obj:
+            for book in obj.readlines():
+                books.add(book)
 
-        books = sorted(set(books), key=books.index)   #去重
-        with open(self.booklist,'w+') as bkObj:
+        with open(self.booklist,'w+',encoding='utf-8') as obj:
             for book in books:
-                bkObj.write(book + '\n')              #再次写入
+                obj.write(book)                       #再次写入
 
     def search(self):
         '''实现逻辑'''
-        if len(sys.argv) != 2:
-            print(f"Usage: python {sys.argv[0]} [xx.epub|xx.mobi]")
-            sys.exit(-1)
-
         cwd = getcwd()
-        zipDir = self.getPrefix(self.epubName)        #获取电子书名以创建同名文件夹
-        self.copy2zipdir(zipDir)                      #复制电子书到同名文件夹 
 
-        chdir(zipDir)                                 #切换到文件夹,开始处理电子书  
+        self.copy2zipdir(self.prefix)                 #复制电子书到同名文件夹 
+        chdir(self.prefix)                            #切换到文件夹,开始处理电子书  
         zipName = self.renameEpub2zip(self.epubName)  #改电子书格式为zip 
         self.unzip(zipName)                           #解压缩zip文件
 
@@ -142,24 +135,26 @@ class EPUBbooksfinder():
         chdir(cwd)                                    #退回当前目录,开始查找书籍任务
 
         for html in self.htmlLst:
-            self.findBook(zipDir+self.delimiter+html) #在(x)html中找书，核心函数  
+            self.findBook(self.prefix+self.delimiter+html) #在(x)html中找书，核心函数  
                                                             
         self.rmduplicate()                            #去除重复记录的书籍,没有则直接返回
-        send2trash(zipDir)                            #删除解压缩得到的文件夹　
+        send2trash(self.prefix)                       #删除解压缩得到的文件夹　
 
-def main():
-    if len(sys.argv) < 2:
-        print(f"Usage: {basename(sys.argv[0])} [xx.epub|xx.mobi] or {basename(sys.argv[0])} -a")
+@click.command()
+@click.option('--ebook', default=None, help='Ebook name like xxx.epub/xxx.mobi or all')
+def getBookList(ebook):
+    if  None == ebook:
+        print(f'Usage: {sys.argv[0]} --ebook=xxx.epub')
         sys.exit(-1)
 
-    if  '-a' == sys.argv[1]:
+    if 'all' != ebook:
+        ebooksfinder = EPUBbooksfinder(ebook)
+        ebooksfinder.search()
+    else:
         for ebook in listdir('.'):
             if ebook.endswith('.epub') or ebook.endswith('.mobi'):
                 ebooksfinder = EPUBbooksfinder(ebook)
                 ebooksfinder.search()
-    else:
-        ebooksfinder = EPUBbooksfinder(sys.argv[1])
-        ebooksfinder.search()
 
 if __name__ == "__main__":
-    main()
+    getBookList()
